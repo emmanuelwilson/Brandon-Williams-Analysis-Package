5c39f09dcb0ded43eab2dde29f80ac3a2c69d4ec
Emmanuel Wilson <emmanuel.kelly.wilson@gmail.com>
2019-12-17 13:08:52 -0500

    delete

--- Touchscreen/raster.m
+++
@@ -1,8 +0,0 @@
-%Sorts and Rasterizes calcium traces from ms 
-function [raster] = raster(ms)
-raster = ms.frame;
-
-[~,maxind] = max(raster,[],2);
-[~, rastsort] = sort(maxind);
-raster = raster(rastsort,:);
-end
--- Touchscreen/sortpeaks.m
+++
@@ -1,7 +0,0 @@
-%Will sort through the raster plot reoganizing cells by time of highest
-%peak and return new raster plot
-function [rast,rastsort] = sortpeaks(rast)
-[~,maxind] = max(rast,[],2);
-[~, rastsort] = sort(maxind);
-rast = rast(rastsort,:);
-end
