684d4de8224423dcba45aad03c7252cd933eb71f
Emmanuel Wilson <emmanuel.kelly.wilson@gmail.com>
2019-12-17 12:56:54 -0500

    delete

--- SingleFunctions/msSegementsInField.m
+++
@@ -1,7 +0,0 @@
-function [ output_args ] = msSegementsInField( input_args )
-%MSSEGEMENTSINFIELD Summary of this function goes here
-%   Detailed explanation goes here
-
-
-end
-
--- SingleFunctions/parsave.m
+++
@@ -1,7 +0,0 @@
-function parsave(fname,ms)
-%PARSAVE Summary of this function goes here
-%   Detailed explanation goes here
-    save(fname,'ms','-v7.3');
-
-end
-
