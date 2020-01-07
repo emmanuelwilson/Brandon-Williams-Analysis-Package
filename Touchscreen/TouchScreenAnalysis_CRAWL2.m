80eb45c0c085e9f763355cefaa62014c792a344f
Emmanuel Wilson <emmanuel.kelly.wilson@gmail.com>
2020-01-07 10:36:11 -0500

    If statement correction

--- Touchscreen/TouchScreenAnalysis_CRAWL2.m
+++ Touchscreen/TouchScreenAnalysis_CRAWL2.m
@@ -9,7 +9,7 @@ for i = 3 : length(folder)
         subdir = [pwd,'\',folder(i).name];
         subfolder = dir(subdir);
         fnames = {subfolder.name};
-        if ~isempty(find(strcmp(fnames,'msTouchSync.mat'),1)) && ~isempty(find(strncmp(fnames,'timestamp.dat',1),1)) && isempty(find(strcmp(fnames,'msDeconvolved.mat'),1))
+        if ~isempty(find(strcmp(fnames,'msTouchSync.mat'),1)) && ~isempty(find(strncmp(fnames,'timestamp.dat',1),1)) && ~isempty(find(strcmp(fnames,'msDeconvolved.mat'),1))
             cd([pwd,'/',folder(i).name]);
             load('msDeconvolved.mat')
             load('msTouchSync.mat')
