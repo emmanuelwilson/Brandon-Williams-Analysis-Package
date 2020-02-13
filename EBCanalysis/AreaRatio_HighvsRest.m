fdcf501df6509af0568aee393dcba9dda9e97844
Emmanuel Wilson <emmanuel.kelly.wilson@gmail.com>
2020-02-13 14:07:05 -0500

    update AUC Ratio function
    Inlcude window size input variable and update window implimentation within the function

--- EBCanalysis/AreaRatio_HighvsRest.m
+++ EBCanalysis/AreaRatio_HighvsRest.m
@@ -1,37 +1,38 @@
 %% Takes ratio from Area under the curve from max peak vs everything else
 
-function [ratios] = AreaRatio_HighvsRest(corrangles)
+function [ratios] = AreaRatio_HighvsRest(corrangles,window)
 maxval = [];
 minval = [];
 ratios = NaN(size(corrangles));
 y = [];
 x = [];
-window = 4;
+% window = 8;
 for i = 1 : length(corrangles(:,1,1))
     if any(any(~cellfun(@isempty,corrangles(i,:,:))))
         corrrots = permute(corrangles(i,:,:),[3 2 1]);
         [y,x] = find(permute(~cellfun(@isempty,corrangles(i,:,:)),[3 2 1]));
         for j = 1 : length(y)
             maxpeak = find(corrrots{y(j),x(j)} == max(corrrots{y(j),x(j)}));
+            minamp = min((corrrots{y(j),x(j)}));
             if maxpeak < window+1
                 topadd = window +1 - maxpeak;
                 botadd = window - topadd;
-                maxval = sum(corrrots{y(j),x(j)}([maxpeak-botadd:maxpeak+window, end - topadd : end]));
+                maxval = sum(corrrots{y(j),x(j)}([maxpeak-botadd:maxpeak+window, end - topadd : end]))-(window*minamp);
                 mincorr = corrrots{y(j),x(j)};
                 mincorr([maxpeak-botadd:maxpeak+window, end - topadd : end]) = [];
-                minval = sum(mincorr);
-            elseif maxpeak > 52
+                minval = sum(mincorr)-(length(mincorr)*minamp);
+            elseif maxpeak > 60 - window
                 botadd = maxpeak + window - 60;
                 topadd = window-botadd;
-                maxval = sum(corrrots{y(j),x(j)}([maxpeak-window:maxpeak+topadd, 1 : botadd]));
+                maxval = sum(corrrots{y(j),x(j)}([maxpeak-window:maxpeak+topadd, 1 : botadd]))-(window*minamp);
                 mincorr = corrrots{y(j),x(j)};
                 mincorr([maxpeak-window:maxpeak+topadd, 1 : botadd]) = [];
-                minval = sum(mincorr);
+                minval = sum(mincorr)-(length(mincorr)*minamp);
             else
-                maxval = sum(corrrots{y(j),x(j)}(maxpeak-window:maxpeak+window));
+                maxval = sum(corrrots{y(j),x(j)}(maxpeak-window:maxpeak+window))-(window*minamp);
                 mincorr = corrrots{y(j),x(j)};
                 mincorr(maxpeak-window:maxpeak+window) = [];
-                minval = sum(mincorr);
+                minval = sum(mincorr)-(length(mincorr)*minamp);
             end
             ratios(i,y(j),x(j)) = minval/maxval;
         end
