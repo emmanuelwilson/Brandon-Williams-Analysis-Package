35221c4591c2339a11c749305a8552e987fb3e20
Emmanuel Wilson <emmanuel.kelly.wilson@gmail.com>
2020-02-18 10:27:58 -0500

    Update rmcorrserialize.m
    added test if statement and fixed indexing shortcomming

--- EBCanalysis/rmcorrserialize.m
+++ EBCanalysis/rmcorrserialize.m
@@ -2,12 +2,18 @@
 
 function [meanval] = rmcorrserialize(vals)
 tempcat = [];
+t = 0;
 for i = 1 : length(vals(:,1,1))    
-    for j = 1 : length(vals(1,:,1))-1
-        if any(~isnan(vals(i,j,:)))            
+    for j = 1 : length(vals(1,:,1))
+        if any(~isnan(vals(i,j,:)))
             tempcat = cat(1,tempcat,permute(vals(i,j,~isnan(vals(i,j,:))),[3 2 1]));
+            t = 1;
         end
     end
+    if ~t
+        testytest =  permute(vals(i,:,:),[3 2 1]);
+    end
+    t = 0;
 end
 
 meanval = tempcat;
