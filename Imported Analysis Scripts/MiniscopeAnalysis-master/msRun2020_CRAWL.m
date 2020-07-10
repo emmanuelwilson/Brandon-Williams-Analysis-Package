5730c2dcb630de4de8cfb259db159ba07f68837e
Emmanuel Wilson <emmanuel.kelly.wilson@gmail.com>
2020-07-10 13:59:01 -0400

    Update msRun2020_CRAWL.m

--- Imported Analysis Scripts/MiniscopeAnalysis-master/msRun2020_CRAWL.m
+++ Imported Analysis Scripts/MiniscopeAnalysis-master/msRun2020_CRAWL.m
@@ -5,14 +5,18 @@ function [] = msRun2020_CRAWL(p)
 
 folder = dir(p);                                                         %List contents of your current folder
 oldCD = pwd;                                                               %Save path to directory
-for i = 3 : length(folder)                                                 %Look through folder items
+parfor i = 3 : length(folder)                                                 %Look through folder items
     if folder(i).isdir == 1                                                %Will proceed only if folder item is another folder/directory
         subdir = [p,'/',folder(i).name];                                 %file path
         subfolder = dir(subdir);                                           %List subfolder contents
         fnames = {subfolder.name};                                         %List subfolder item names
         if isempty(find(strcmp(fnames,'ms.mat'),1)) && ~isempty(find(strncmp(fnames,'timestamp',1),1)) %Will proceed if no structure is present and timestamp file being present
             cd([p,'/',folder(i).name]);                                  %Change current folder
-            msRun2020(pwd)                                                      %Run analysis
+            try
+                msRun2020(pwd)                                                      %Run analysis
+            catch
+                display([p,'/',folder(i).name,' Failed to analize'])
+            end
             cd(oldCD);                                                     %Return
             
         else
@@ -23,6 +27,11 @@ for i = 3 : length(folder)                                                 %Look
                     fnames2 = {subfolder2.name};                                         %List subfolder item names
                     if isempty(find(strcmp(fnames2,'ms.mat'),1)) && ~isempty(find(strncmp(fnames2,'timestamp',1),1)) %Will proceed if no structure is present and timestamp file being present
                         cd([p,'/',folder(i).name,'/',subfolder(j).name]);                                  %Change current folder
+                        try
+                            msRun2020(pwd)                                                      %Run analysis
+                        catch
+                            display([p,'/',folder(i).name,'/',subfolder(j).name,' Failed to analize'])
+                        end
                         msRun2020(pwd)                                                  %Run analysis
                         cd(oldCD);
                     end
