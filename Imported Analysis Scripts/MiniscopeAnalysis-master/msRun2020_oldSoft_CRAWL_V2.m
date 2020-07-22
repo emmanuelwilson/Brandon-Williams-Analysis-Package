96e0c5fb781059d98a833490d439ea4be416c701
Emmanuel Wilson <emmanuel.kelly.wilson@gmail.com>
2020-07-22 10:29:46 -0400

    Create msRun2020_oldSoft_CRAWL_V2.m

---
+++ Imported Analysis Scripts/MiniscopeAnalysis-master/msRun2020_oldSoft_CRAWL_V2.m
@@ -0,0 +1,26 @@
+%% Crawl script for ms structure creation
+%Must have all wanted folders and subfolders to run CNMFE on in the given path
+
+function [] = msRun2020_oldSoft_CRAWL_V2(p)
+paths = genpath(p);
+if ispc
+    folders = strsplit(paths,';')';
+else
+    folders = strsplit(paths,':')';
+end
+for i = 1 : length(folders)
+    folders{i}
+    if ~isempty(folders{i})        
+        d = dir(folders{i});
+        fnames = {d.name};
+        if isempty(find(strcmp(fnames,'ms.mat'),1)) && ~isempty(find(strncmp(fnames,'timestamp',1),1))
+            cd(folders{i});                                  %Change current folder
+            try
+                msRun2020(pwd)                                                      %Run analysis
+            catch
+                display([folders{i},' Failed to analize'])
+            end            
+        end
+    end
+end
+end
