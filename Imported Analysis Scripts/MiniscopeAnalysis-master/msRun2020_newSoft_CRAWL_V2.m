5693b2b680d6705b91cbeb0fd4a92ecbd32084f1
Emmanuel Wilson <emmanuel.kelly.wilson@gmail.com>
2020-10-21 13:26:54 -0400

    Update msRun2020_newSoft_CRAWL_V2.m

--- Imported Analysis Scripts/MiniscopeAnalysis-master/msRun2020_newSoft_CRAWL_V2.m
+++ Imported Analysis Scripts/MiniscopeAnalysis-master/msRun2020_newSoft_CRAWL_V2.m
@@ -8,11 +8,13 @@ if ispc
 else
     folders = strsplit(paths,':')';
 end
-for i = 1 : length(folders)
+folders
+for i = 1 : length(folders)    
     if ~isempty(folders{i})        
+        folders{i}
         d = dir(folders{i});
         fnames = {d.name};
-        if isempty(find(strcmp(fnames,'ms.mat'),6)) && ~isempty(find(strncmp(fnames,'timeStamp',9),1)) && ~isempty(find(contains(folders{i},'Miniscope'),1))
+        if isempty(find(strcmp(fnames,'ms.mat'),1)) && ~isempty(find(contains(folders{i},'Miniscope'),1)) %&& ~isempty(find(strncmp(fnames,'timeStamps.csv',9),1)) && ~isempty(find(contains(folders{i},'Miniscope'),1))
             cd(folders{i});                                  %Change current folder
             try
                 msRun2020_newSoft(pwd)                                                      %Run analysis
